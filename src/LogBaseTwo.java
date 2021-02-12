ef9940a867362adb640098693ba564b827680418
inn_user <innapetryshyn@gmail.com>
2021-02-12 15:09:18 +0200

    add print

--- src/LogBaseTwo.java
+++ src/LogBaseTwo.java
@@ -1,11 +1,15 @@
 public class LogBaseTwo {
 
     public static int nlog2(int n) {
+		System.out.println("Calculate Log ");
         return n * log2(n);
     }
 
     public static void main(String[] args) {
         int N = 4;
+		if(N>10){
+		System.out.println(" Log > 10");
+		}
         System.out.println("Log " + N + " to the base 2 = " + log2(N));
         System.out.println("Log " + N + " to the base 2 = " + N * log2(N));
     }
